2a941b83192aaeb03dc1a814085b53cd86722510
Muhammad Tahsin Amin <muhammadtahsinamin@gmail.com>
2022-01-06 13:12:08 +0600

    Adding Fonts

--- components/Layout.js
+++ components/Layout.js
@@ -6,19 +6,19 @@ export default function Layout({ children }) {
     <div>
       <Head>
         <title>ReactJS Assignment</title>
-        {/* <link
-rel="stylesheet"
-href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.2.0-rc/css/adminlte.min.css"
-integrity="sha512-YOsl4pnOb5NC868yn1JxAzjJsWkLNtP53uc3OcyAl0Q2R1cwo/mdI1hHSQM8gbIxWj97mKeLoD9R0aiYibFQAA=="
-crossOrigin="anonymous"
-referrerPolicy="no-referrer"
-/> */}
-        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-<link
-href="https://fonts.googleapis.com/css2?family=Catamaran:wght@800&family=Montserrat:wght@300&display=swap"
-rel="stylesheet"
-/> */}
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.2.0-rc/css/adminlte.min.css"
+          integrity="sha512-YOsl4pnOb5NC868yn1JxAzjJsWkLNtP53uc3OcyAl0Q2R1cwo/mdI1hHSQM8gbIxWj97mKeLoD9R0aiYibFQAA=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Catamaran:wght@800&family=Montserrat:wght@300&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       {/* <Navbar /> */}
 
